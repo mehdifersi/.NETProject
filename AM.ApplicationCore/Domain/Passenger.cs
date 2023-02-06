8f8f42c39e1a3b077f6cdeaed1463703f960b9ef
mehdifersi <mahdi.fersi@esprit.tn>
2023-02-06 14:02:26 +0100

    TP1

---
+++ AM.ApplicationCore/Domain/Passenger.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AM.ApplicationCore.Domain
+{
+    public class Passenger
+    {
+        public int PasseportNumber { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }    
+        public string PhoneNumber { get; set; }
+        public DateTime BirthDate { get; set; }
+        public ICollection<Flight> Flights { get; set; }
+
+        //public bool CheckProfile (string first_name,string last_name)
+        //{
+        //    if(FirstName == first_name && LastName == last_name)
+        //        return true;
+        //    return false;
+            
+        //}
+        //public bool CheckProfile(string first_name, string last_name,string email)
+        //{
+        //    if (EmailAddress==email && FirstName == first_name && LastName == last_name)
+        //        return true;
+        //    return false;
+
+        //}
+        public bool CheckProfile(string first_name, string last_name, string email=null)
+        {
+            if (email == null)
+            {
+                if (FirstName == first_name && LastName == last_name)
+                    return true;
+                return false;
+            }
+            else 
+            {
+                if (EmailAddress == email && FirstName == first_name && LastName == last_name)
+                    return true;
+                return false;
+            }
+        }
+        public virtual void PassengerType()
+        {
+            Console.WriteLine("I'm a passenger");
+        }
+
+
+    }
+
+}
---
+++ AM.UI.Console/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AM.ApplicationCore.Domain;
+
+Console.WriteLine("Hello, World!");
+//Plane plane1 = new Plane();
+//plane1.Capacity= 200;
+//plane1.PlaneID = 1;
+//plane1.ManufactureDate = new DateTime(2023,02,02);
+//plane1.PlaneType = PlaneType.Boing;
+
+
+//Plane plane2 =new Plane(PlaneType.Airbus,350,new DateTime(2023,01,31));
+
+
+Plane plane3 = new Plane()
+{
+    Capacity = 100,
+    PlaneType = PlaneType.Boing,
+    ManufactureDate = DateTime.Now
+};
+
+Passenger passenger1 = new Passenger()
+{
+      FirstName= "Test1",
+      LastName= "Test2",
+      EmailAddress="Test@test.test"
+};
+
+Staff staff1 = new Staff();
+
+Traveller traveller1 = new Traveller();
+
+Console.WriteLine(passenger1.CheckProfile("Test1", "Test2", "Test@test.test"));
+
+staff1.PassengerType();
+
+traveller1.PassengerType();
